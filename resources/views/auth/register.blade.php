5c2752fd9a9395bc004906feb8c9e7732aabfe9b
n-kanae <kanae.nakatsuji@gmail.com>
2023-03-26 15:04:09 +0900

    新規登録/バリデーションの適用_230326 フォームファサードが使用できるようにapp.php編集

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,25 +4,26 @@
 
 {!! Form::open() !!}
 
+<div class="container">
 <h2>新規ユーザー登録</h2>
 
 {{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+{{ Form::text('username',null,['class' => 'input'],'placeholder' => 'dawntown') }}
 
 {{ Form::label('メールアドレス') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
+{{ Form::email('mail',null,['class' => 'input','placeholder' => 'dawn@dawn.jp']) }}
 
 {{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
+{{ Form::password('password',null,['class' => 'input']) }}
 
 {{ Form::label('パスワード確認') }}
-{{ Form::text('password-confirm',null,['class' => 'input']) }}
+{{ Form::password('password-confirm',null,['class' => 'input']) }}
 
 {{ Form::submit('登録') }}
 
 <p><a href="/login">ログイン画面へ戻る</a></p>
 
 {!! Form::close() !!}
-
+</div>
 
 @endsection
