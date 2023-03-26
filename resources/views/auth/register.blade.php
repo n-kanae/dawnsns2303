4c404872957d29363cc56300096c789d4071f6c1
n-kanae <kanae.nakatsuji@gmail.com>
2023-03-26 15:19:47 +0900

    フォームファサード修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,19 +7,19 @@
 <div class="container">
 <h2>新規ユーザー登録</h2>
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input'],['placeholder' => 'dawntown']) }}
+{!! Form::label('ユーザー名') !!}
+{!! Form::text('username',null,['class' => 'input'],['placeholder' => 'dawntown']) !!}
 
-{{ Form::label('メールアドレス') }}
-{{ Form::email('mail',null,['class' => 'input',['placeholder' => 'dawn@dawn.jp']) }}
+{!! Form::label('メールアドレス') !!}
+{!! Form::email('mail',null,['class' => 'input',['placeholder' => 'dawn@dawn.jp']) !!}
 
-{{ Form::label('パスワード') }}
-{{ Form::password('password',null,['class' => 'input']) }}
+{!! Form::label('パスワード') !!}
+{!! Form::password('password',null,['class' => 'input']) !!}
 
-{{ Form::label('パスワード確認') }}
-{{ Form::password('password-confirm',null,['class' => 'input']) }}
+{!! Form::label('パスワード確認') !!}
+{!! Form::password('password-confirm',null,['class' => 'input']) !!}
 
-{{ Form::submit('登録') }}
+{!! Form::submit('登録') !!}
 
 <p><a href="/login">ログイン画面へ戻る</a></p>
 
