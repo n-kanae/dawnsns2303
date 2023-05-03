77b52fe201012282d472bb4b11104544bb3ba696
LAPTOP-OCJS2M1N\pckas <kane.nakatsuji@gmail.com>
2023-05-03 19:42:55 +0900

    モデルの削除

--- app/User.php
+++ app/User.php
@@ -26,36 +26,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function followers()
-    {
-        return $this->belongsToMany(self::class, 'follows', 'follow_id', 'follower_id')
-                    ->withTimestamps();
-    }
-
-    public function follows()
-    {
-        return $this->belongsToMany(self::class, 'follows', 'follower_id', 'follow_id')
-                    ->withTimestamps();
-    }
-
-    public function follow($user_id)
-    {
-        return $this->follows()->attach($user_id);
-    }
-
-    public function unfollow($user_id)
-    {
-        return $this->follows()->detach($user_id);
-    }
-
-    public function isFollowing($user_id)
-    {
-        return (boolean) $this->follows()->where('follower_id', $user_id)->exists();
-    }
-
-    public function isFollowed($user_id)
-    {
-        return (boolean) $this->followers()->where('follow_id', $user_id)->exists();
-    }
 }
